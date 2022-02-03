c4fb654b6a73e7215a0173e977bbee70e6602636
Ignacio Ordenes <e-iordenesp@ripley.com>
2022-02-02 21:50:47 -0300

    Se agregan archivos groovy

---
+++ vars/ejecucion.groovy
@@ -0,0 +1,37 @@
+def call(){
+  pipeline {
+      agent any
+      environment {
+          NEXUS_USER         = credentials('NEXUS-USER')
+          NEXUS_PASSWORD     = credentials('NEXUS-PASS')
+      }
+      parameters {
+            choice choices: ['maven', 'gradle'], description: 'Seleccione una herramienta para preceder a compilar', name: 'compileTool'
+            text description: 'Enviar los stages separados por ";"... Vacío si necesita todos los stages', name: 'stages'
+      }
+      stages {
+          stage("Pipeline"){
+              steps {
+                  script{
+                      sh "env"
+                      env.TAREA = ""
+                      if(params.compileTool == 'maven'){
+                        maven.call(params.stages);
+                      }else{
+                        gradle.call(params.stages)
+                      }
+                  }
+              }
+              post{
+                success{
+                    slackSend color: 'good', message: "[Ignacio] [${JOB_NAME}] [${BUILD_TAG}] Ejecucion Exitosa", teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: 'token-slack'
+                }
+                failure{
+                    slackSend color: 'danger', message: "[Ignacio] [${env.JOB_NAME}] [${BUILD_TAG}] Ejecucion fallida en stage [${env.TAREA}]", teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: 'token-slack'
+                }
+            }
+          }
+      }
+  }
+}
+return this;
