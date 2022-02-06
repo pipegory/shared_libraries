ffe01a0b7782666440839ebb3a2fa76e0e1a6936
Ignacio Ordenes <e-iordenesp@ripley.com>
2022-02-06 03:54:52 -0300

    se incrementa el tiempo

--- vars/dc.groovy
+++ vars/dc.groovy
@@ -28,8 +28,8 @@ def stageRunJar(){
 def stageRunTest(){
     env.TAREA="Paso 8: Ejecución Test URLs"
     stage("$env.TAREA"){
-        sh "nohup mvnw spring-boot:run &"
-        sh "sleep 20"
+        sh "nohup bash mvnw spring-boot:run &"
+        sh "sleep 60"
         validaStatus('http://localhost:8081/rest/mscovid/test?msg=testing')
         validaStatus('http://localhost:8081/rest/mscovid/estadoMundial')
         validaStatus('http://localhost:8081/rest/mscovid/estadoPais?pais=chile')
